683a1d731bf2d45a94bf705a5f955c9652a4cccd
reginaldshub <reginald.anthony@accionlabs.com>
2018-12-17 18:14:14 +0530

    commands

---
+++ init_geth.sh
@@ -0,0 +1,2 @@
+geth --datadir  "tony-a" --port "30304" --nodiscover  --rpcapi "db,eth,net,web3,personal,txpool" --rpc --rpcport "8545" --rpcaddr "127.0.0.1" --rpccorsdomain="*" --networkid 1900  --nat "any" console
+
